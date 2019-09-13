05c8586ac4c84150314896e12984af97f805dc50
Martin Bozzo <mbozzo@salesforce.com>
2019-09-13 11:07:41 -0300

    Adding App.js and like button component

---
+++ stories/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+storiesOf('Welcome', module).add('to Storybook', function () {
+  return React.createElement(Welcome, { showApp: linkTo('Button') });
+});
+
+storiesOf('Button', module).add('with text', function () {
+  return React.createElement(
+    Button,
+    { onClick: action('clicked') },
+    'Hello Button'
+  );
+}).add('with some emoji', function () {
+  return React.createElement(
+    Button,
+    { onClick: action('clicked') },
+    React.createElement(
+      'span',
+      { role: 'img', 'aria-label': 'so cool' },
+      '\uD83D\uDE00 \uD83D\uDE0E \uD83D\uDC4D \uD83D\uDCAF'
+    )
+  );
+});
